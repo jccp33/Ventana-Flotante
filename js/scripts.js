47efcfbeb43a8a9b00e76d33830c3a168591e26c
Carlos Cárdenas <jccp33@hotmail.com>
2019-08-26 22:52:01 -0700

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+
+function WindowsMax(){
+	document.getElementById('w-float').style.height = "300px";
+	document.getElementById('w-content').style.display = "block";
+}
+
+function WindowsMin(){
+	document.getElementById('w-float').style.height = "20px";
+	document.getElementById('w-content').style.display = "none";
+}
